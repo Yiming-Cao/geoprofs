b4dfda1ecc47f93a19891f5af3cbfa02596ef996
TvMroc <1207837@student.roc-nijmegen.nl>
2025-09-26 12:51:17 +0200

    remove debug banner

--- lib/main.dart
+++ lib/main.dart
@@ -21,6 +21,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'GeoProfs',
+      debugShowCheckedModeBanner: false,
       initialRoute: '/',
       routes: {
         '/': (context) => const HomeScreen(),
